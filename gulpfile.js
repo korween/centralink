94c49ac8728b23fd2ba9cc21f0ea0ef531b8b2b5
Korween <kortechmail@gmail.com>
2016-01-05 07:42:59 +0100

    -- Added Flat-UI for Bootstrap - Changed to build system to accomodate for the new library - Began working on the UI styling. (Notification view)

--- gulpfile.js
+++ gulpfile.js
@@ -2,30 +2,45 @@ var gulp = require('gulp');
 var browserify = require('browserify');
 var concat = require('gulp-concat');
 var sass = require('gulp-sass');
+var less = require('gulp-less');
+var clean = require('gulp-clean');
 var refresh = require('gulp-livereload');
 var lr = require('tiny-lr');
-var server = lr();
 var minifyCSS = require('gulp-minify-css');
 var embedlr = require('gulp-embedlr');
 var bulkify = require('bulkify');
-
 var source = require('vinyl-source-stream');
 var buffer = require('vinyl-buffer');
 var ngAnnotate = require('gulp-ng-annotate');
 var uglify = require('gulp-uglify');
 
-var vendorJS=[
+var server = lr();
+
+
+/* Vendor JS files are compiled and resolved by browserify */
+
+var vendorJS = [
     './node_modules/angular/angular.min.js',
     './node_modules/angular-ui-router/release/angular-ui-router.min.js'
 ];
 
-var vendorCSS=[
+/* Vendor CSS files are minified into vendor.css and placed into dist/assets/css/vendor.css */
 
+var vendorCSS = [
+    './node_modules/flat-ui/bootstrap/css/bootstrap.css',
+    //'./node_modules/flat-ui/css/flat-ui.css'
 ];
 
-var opts= {entries: './app/src/app.js',transform: ['bulkify']}
+/* Vendor assets are copied as is into dist/assets/ */
+
+var vendorAssets = [
+    './node_modules/flat-ui/fonts/**/*'
+]
+
+/* This part looks hackish but it's actually pretty clean for what it does */
 
 gulp.task('scripts', function() {
+    var opts= {entries: './app/src/app.js',transform: ['bulkify']}
     var b = browserify(opts);
     b.transform('bulkify',{});
 
@@ -50,11 +65,57 @@ gulp.task('vendorJS', function() {
 });
 
 gulp.task('styles', function() {
-    gulp.src(['app/src/styles/styles.scss'].concat(vendorCSS))
+    gulp.src(['app/src/styles/styles.scss'])
         .pipe(sass())
         .pipe(minifyCSS())
-        .pipe(gulp.dest('dist'))
+        .pipe(gulp.dest('./dist/assets/css/'))
         .pipe(refresh(server))
+})
+
+/*
+   This is custom, it allows to change the colors provided by flat-ui manually
+   The new file has not been included into vendor.js, to prevent confusion
+   and avoid things like merging streams. You'll thank me later.
+*/
+
+gulp.task('flatUILess',function() {
+    /* Question: why don't those run in async and throw an error? */
+    gulp.src([
+        './node_modules/flat-ui/less/**/*',
+        '!./node_modules/flat-ui/less/variables.less',
+        './app/vendor/variables.less'
+    ]).pipe(gulp.dest('./tmp/'));
+
+    gulp.src('./tmp/flat-ui.less')
+        .pipe(less())
+        .pipe(minifyCSS())
+        .pipe(gulp.dest('./dist/assets/css/'))
+        .pipe(refresh(server))
+
+    /* Answer: no idea. Gulp might use WriteFileSync after all :) */
+})
+
+gulp.task('vendorCSS',function() {
+    gulp.src(vendorCSS)
+        .pipe(concat('vendor.css'))
+        .pipe(minifyCSS())
+        .pipe(gulp.dest('./dist/assets/css/'))
+        .pipe(refresh(server))
+    })
+
+gulp.task('vendorAssets',function() {
+    /* Hacking again? */
+    var assetPath;
+    for(var a in vendorAssets) {
+        assetPath = vendorAssets[a].split('\*')[0].split('/');
+        assetPath.pop();
+        assetPath.pop();
+        assetPath=assetPath.join('/');
+
+        gulp.src(vendorAssets[a],{'base':assetPath})
+            .pipe(gulp.dest('./dist/assets/'))
+    }
+    /* Yup */
 })
 
 gulp.task('lr-server', function() {
@@ -75,9 +136,10 @@ gulp.task('html', function() {
 })
 
 gulp.task('default', function() {
-    gulp.run('vendorJS','lr-server', 'scripts', 'styles', 'html');
+    gulp.run('flatUILess','vendorAssets','vendorCSS','vendorJS','lr-server', 'scripts', 'styles', 'html');
 
     gulp.watch('app/src/**', ['scripts'])
+    gulp.watch('app/vendor/variables.less', ['flatUILess'])
     gulp.watch('app/css/**', ['styles'])
     gulp.watch('app/**/*.html', ['html'])
 })
